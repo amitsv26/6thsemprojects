f243881074ebc8a1614cc681d391bc7124221d8e
Amit Kumar Gupta <31799004+amitsv26@users.noreply.github.com>
2020-04-23 17:25:19 +0530

    Add files via upload

---
+++ cg/include/darts.h
@@ -0,0 +1,20 @@
+#ifndef DARTS_H
+#define DARTS_H
+
+#include "point.h"
+#include "shape.h"
+#include "text.h"
+#include "circle.h"
+#include "rectangle.h"
+
+// Global constants
+const int ANIMATION_MSEC = 5;
+
+// Function prototypes
+void reshape(int width, int height);
+void display();
+void getKeyboardInput(unsigned char c, int x, int y);
+void animate(int val);
+void updateScore(float dartY);
+
+#endif // DARTS_H
